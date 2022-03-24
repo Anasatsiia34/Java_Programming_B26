eef66b6156cbce9e70298f1e57969266d1584b77
user <anastasiia34144@gmail.com>
2022-03-24 19:35:55 -0400

    new

--- src/my_utilities/StringUtil.java
+++ src/my_utilities/StringUtil.java
@@ -1,4 +1,17 @@
 package my_utilities;
 
 public class StringUtil {
+    public static String reverse(String str){
+        String reversed = " ";
+        for(int i = str.length() -1; i >= 0; i--){
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse("java"));
+    }
+
 }
